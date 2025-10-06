3b9253b970999ffa96473c3d3d669f666d9a6ff1
andreiv1 <vasilieviciandrei01@gmail.cm>
2025-10-06 16:25:39 +0300

    project setup

---
+++ src/main/kotlin/net/awslink/homepage_provision/HomepageProvisionApplication.kt
@@ -0,0 +1,11 @@
+package net.awslink.homepage_provision
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HomepageProvisionApplication
+
+fun main(args: Array<String>) {
+	runApplication<HomepageProvisionApplication>(*args)
+}
